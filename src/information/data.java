daf2d88f49c485026b6afac0549badf34b054e11
JamesRojes <briziljam@gmail.com>
2022-05-19 13:36:30 +0530

    new code

---
+++ src/information/data.java
@@ -0,0 +1,61 @@
+package information;
+
+public class data{
+public static void main(String[] args) {
+byte age= 22;	
+System.out.println(" Age is"+age);
+short id = 6880;
+System.out.println("Id is "+id);
+int pin=60001;
+System.out.println("Pincode is "+pin);
+long mob =9876543;
+System.out.println("Mobile is"+mob);
+float a =9500.58f;
+System.out.println("Height is"+a);
+double sal=9500.587;
+System.out.println("Salary is "+sal);
+char gender ='M';
+System.out.println("Gender is"+gender); 
+String s="Arun";
+System.out.println("Name is "+s);
+boolean m=false;
+System.out.println("Marital status is"+m);
+
+
+
+
+}
+
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
